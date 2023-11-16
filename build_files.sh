7dea64070913b9ee8078542a7a44a21284882089
jesuiszed <151054516+jesuiszed@users.noreply.github.com>
2023-11-16 18:57:23 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,8 @@
- echo "BUILD START"
- python3.9 -m pip install -r requirements.txt
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+echo "BUILD START"
+python3.9 -m pip install -r requirements.txt
+
+# Ajout de cette ligne pour créer le répertoire staticfiles s'il n'existe pas
+mkdir -p staticfiles
+
+python3.9 manage.py collectstatic --noinput --clear
+echo "BUILD END"
